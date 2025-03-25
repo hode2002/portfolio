8bb3828f745a0806a071e9eec4e9acd5891398fe
hode2002 <hovande.dev.2002@gmail.com>
2025-03-25 13:18:23 +0700

    refactor: move viewport settings to a separate file for better organization

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -68,12 +68,6 @@ export const metadata: Metadata = {
     ],
     siteName: "Hồ Văn Dễ Portfolio",
   },
-  viewport: {
-    width: "device-width",
-    initialScale: 1,
-    maximumScale: 1,
-    userScalable: false,
-  },
   alternates: {
     canonical: "https://hovande.id.vn",
     languages: {
