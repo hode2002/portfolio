435a897dd3b38cb83ad376c4c0d4d603e99d3560
hode2002 <hovande.dev.2002@gmail.com>
2025-03-25 13:31:01 +0700

    refactor: add viewport settings to layout.tsx for improved responsiveness

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
@@ -78,6 +78,13 @@ export const metadata: Metadata = {
     google: "IoUEHtSznyVv3eWlehBPN2EBan1_yPdSFcBBABDHW3s"
   },
   category: "Developer",
+};
+
+export const viewport: Viewport = {
+  width: "device-width",
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
 };
 
 export default function RootLayout({
